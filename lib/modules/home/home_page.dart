e1d12f1a3a5bf679bcf8fed0eee6b8a9705d4e3a
Fernando Miguel <buguel2000@gmail.com>
2022-05-26 10:50:18 -0300

    feat: login

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
+import '../../shared/themes/app_colors.dart';
+import '../../shared/themes/app_text_styles.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                    text: "Olá, ",
+                    style: TextStyles.titleRegular,
+                    children: [
+                      TextSpan(
+                          text: "Miguel", style: TextStyles.titleBoldBackground)
+                    ]),
+              ),
+              subtitle: Text(
+                "Mantenha suas contas em dia",
+                style: TextStyles.captionShape,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import '../../shared/auth/auth_controller.dart';
+
 class LoginController {
+  final authController = AuthController();
   Future<void> googleSignIn(BuildContext context) async {
     print("Google Sign In");
     GoogleSignIn _googleSignIn = GoogleSignIn(
@@ -11,8 +14,10 @@ class LoginController {
     );
     try {
       final response = await _googleSignIn.signIn();
+      authController.setUser(context, response);
       print(response);
     } catch (error) {
+      authController.setUser(context, null);
       print(error);
     }
   }
