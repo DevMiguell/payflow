fd73080e83bd3f9f8be70dc7664a16b84095f9ef
Fernando Miguel <buguel2000@gmail.com>
2022-05-27 06:49:44 -0300

    feat: init control pages in home

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../../shared/models/user_model.dart';
 import '../../shared/themes/app_colors.dart';
 import '../../shared/themes/app_text_styles.dart';
+import 'home_controller.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  // final UserModel user;
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+  final pages = [
+    Container(color: Colors.red),
+    Container(color: Colors.green),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +43,66 @@ class HomePage extends StatelessWidget {
                 "Mantenha suas contas em dia",
                 style: TextStyles.captionShape,
               ),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(5),
+                  // image: DecorationImage(
+                  //     image: NetworkImage(widget.user.photoURL!)),
+                ),
+              ),
             ),
           ),
+        ),
+      ),
+      body: pages[controller.currentPage],
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+                onPressed: () {
+                  controller.setPage(0);
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.home,
+                  color: controller.currentPage == 0
+                      ? AppColors.primary
+                      : AppColors.body,
+                )),
+            GestureDetector(
+              onTap: () async {
+                await Navigator.pushNamed(context, "/barcode_scanner");
+                setState(() {});
+              },
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(5)),
+                child: const Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.background,
+                ),
+              ),
+            ),
+            IconButton(
+                onPressed: () {
+                  controller.setPage(1);
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.description_outlined,
+                  color: controller.currentPage == 1
+                      ? AppColors.primary
+                      : AppColors.body,
+                ))
+          ],
         ),
       ),
     );
